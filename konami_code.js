d36467f4b2eebe5778faca40f05182f80f4f9b35
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-27 22:39:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,19 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-let index = 0;
+function init() {
+  let index = 0;
 
-function init(e) {
-  const key = parseInt(e.detail || e.which);
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
 
-  if (key === alphabet[index]) {
-    index++;
+    if (key === alphabet[index]) {
+      index++;
 
-    if (index === alphabet.length) {
-      alert("Hurray!");
+      if (index === alphabet.length) {
+        alert("Hurray!");
 
+        index = 0;
+      }
+    } else {
       index = 0;
     }
-  } else {
-    index = 0;
-  }
+  });
 }
+
+init();
