ce1999b7dac75741dd088d29aa6f3d7ef6b036fc
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-27 22:40:11 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ function init() {
   let index = 0;
 
   document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.detail || e.which);
+    const key = parseInt(e.which || e.detail);
 
     if (key === alphabet[index]) {
       index++;
